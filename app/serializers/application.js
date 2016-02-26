80787a41f3bb248c5204f4bb6427ec0027c2d0a6
nrosella <ninorosella@gmail.com>
2016-02-26 22:19:41 +0000

    added notebooks and mock server data

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
-});
+// export default DS.JSONAPISerializer.extend({
+// });
+
+export default DS.RESTSerializer.extend({});
